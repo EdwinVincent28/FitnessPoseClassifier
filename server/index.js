26753a34cf50bc005041a4179c2d1a4771397686
EdwinVincent28 <edwinvincent.evt@gmail.com>
2025-06-25 01:56:58 +0200

    Connected with mongoDB and Cors enabled

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost:27017/pose-classifier-db')
 
 app.get('/hello', (req, res) => {
     res.send('Hello World')
