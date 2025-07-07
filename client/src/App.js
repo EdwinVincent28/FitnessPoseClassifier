380947badfbd98b2b92454514f32c3af6a470c5b
EdwinVincent28 <edwinvincent.evt@gmail.com>
2025-07-07 20:21:34 +0200

    Authorization Context Implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PoseClassifier from './PoseClassifier';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h2>Pose Classifier</h2>
-      <PoseClassifier />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pose" element={<PoseClassifier />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
